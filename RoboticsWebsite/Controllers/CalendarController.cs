54c5f92d1678dfacee71ab8535e0a88394bc7cb6
TeamNumberOne <bradley.paul@uwlax.edu>
2015-10-07 17:17:39 -0500

    Updated CalendarController

---
+++ RoboticsWebsite/Controllers/CalendarController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RoboticsWebsite.Models;
+
+namespace RoboticsWebsite.Controllers
+{
+    public class CalendarController : Controller
+    {
+        // GET: Calendar
+        [HttpGet]
+        public ActionResult Month()
+        {
+            CalendarViewModel calViewModel = new CalendarViewModel();
+            //calViewModel.GetEvents();
+            return View(calViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Month(CalendarViewModel calViewModel)
+        {
+            //calViewModel.Events.Add(calViewModel.NewEvent);
+            return View(calViewModel);
+        }
+
+        [HttpGet]
+        public ActionResult FullDay()
+        {
+            return View();
+        }
+    }
+}
